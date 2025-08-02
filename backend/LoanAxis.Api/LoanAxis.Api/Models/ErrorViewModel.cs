d147d338f1d9d190193b28f422ca45f4f4366efa
Clifford Hepplethwaite <cliffordrh@gmail.com>
2025-08-02 09:58:35 +0200

    initial commit

---
+++ backend/LoanAxis.Api/LoanAxis.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoanAxis.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
